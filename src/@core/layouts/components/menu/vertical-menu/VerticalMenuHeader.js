514a0389ff6fc8c6452204cc61f69f718b861e08
Rodrigo Cesar Panini <rodrigo_panini@hotmail.com>
2022-10-27 15:25:52 -0300

    ajuste logo

--- src/@core/layouts/components/menu/vertical-menu/VerticalMenuHeader.js
+++ src/@core/layouts/components/menu/vertical-menu/VerticalMenuHeader.js
@@ -1,9 +1,6 @@
 // ** React Imports
 import { useEffect } from "react"
 
-// ** Icons Imports
-import { Disc, X, Circle } from "react-feather"
-
 // ** Config
 import themeConfig from "@configs/themeConfig"
 
@@ -12,7 +9,7 @@ import { getUserData } from "@utils"
 
 const VerticalMenuHeader = (props) => {
   // ** Props
-  const { menuCollapsed, setMenuCollapsed, setGroupOpen, menuHover } = props
+  const { menuCollapsed, setGroupOpen, menuHover } = props
 
   // ** Vars
   const user = getUserData()
@@ -22,43 +19,20 @@ const VerticalMenuHeader = (props) => {
     if (!menuHover && menuCollapsed) setGroupOpen([])
   }, [menuHover, menuCollapsed])
 
-  // ** Menu toggler component
-  const Toggler = () => {
-    if (!menuCollapsed) {
-      return (
-        <Disc
-          size={20}
-          data-tour="toggle-icon"
-          className="text-primary toggle-icon d-none d-xl-block"
-          onClick={() => setMenuCollapsed(true)}
-        />
-      )
-    } else {
-      return (
-        <Circle
-          size={20}
-          data-tour="toggle-icon"
-          className="text-primary toggle-icon d-none d-xl-block"
-          onClick={() => setMenuCollapsed(false)}
-        />
-      )
-    }
-  }
-
   return (
-    <div className="navbar-header">
-      <ul className="nav navbar-nav text-center" style={{ height: "100%" }}>
+    <div className="navbar-header pe-1 ps-1">
+      <ul className="nav navbar-nav" style={{ height: "100%" }}>
         <li className="nav-item">
           <img
-            className="pt-1"
             src={
               themeConfig.layout.skin === "dark"
                 ? user.url_logo_dark
                 : user.url_logo_light
             }
             width="100%"
-            height="100%"
+            height="53"
             alt="logo"
+            className="img-proporcional"
           />
         </li>
       </ul>
