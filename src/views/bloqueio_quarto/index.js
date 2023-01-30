4c82afa56bc77927233b82b4a82f9ebcd6c417d2
Murilo Fernandes <muriloflimaa@gmail.com>
2023-01-30 12:20:46 -0300

    Adicionando tradução até Filtros

--- src/views/bloqueio_quarto/index.js
+++ src/views/bloqueio_quarto/index.js
@@ -1,11 +1,12 @@
 // ** React
-import { Link } from "react-router-dom"
-import { useRef, useState, useEffect, useContext } from "react"
+import { Link } from 'react-router-dom'
+import { useRef, useState, useEffect, useContext } from 'react'
 
 // ** Terceiros
-import ReactPaginate from "react-paginate"
-import { ChevronDown, Eye, Trash, MoreVertical } from "react-feather"
-import DataTable from "react-data-table-component"
+import ReactPaginate from 'react-paginate'
+import { ChevronDown, Eye, Trash, MoreVertical } from 'react-feather'
+import DataTable from 'react-data-table-component'
+import { useTranslation } from 'react-i18next'
 
 // ** Reactstrap
 import {
@@ -20,35 +21,36 @@ import {
   DropdownToggle,
   UncontrolledTooltip,
   UncontrolledDropdown,
-} from "reactstrap"
+} from 'reactstrap'
 
 // ** Store & Actions
-import { getBloqueioQuarto, deleteBloqueioQuarto } from "./store"
-import { useDispatch, useSelector } from "react-redux"
+import { getBloqueioQuarto, deleteBloqueioQuarto } from './store'
+import { useDispatch, useSelector } from 'react-redux'
 
 // ** Styles
-import "@styles/react/libs/tables/react-dataTable-component.scss"
+import '@styles/react/libs/tables/react-dataTable-component.scss'
 
 // ** Context
-import { AbilityContext as PermissaoContext } from "@src/utility/context/Can"
+import { AbilityContext as PermissaoContext } from '@src/utility/context/Can'
 
 // ** Third Party Components
-import Swal from "sweetalert2"
-import withReactContent from "sweetalert2-react-content"
-import toast from "react-hot-toast"
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+import toast from 'react-hot-toast'
 
 const MySwal = withReactContent(Swal)
 
 const CustomHeader = ({ handleFilter, value, handlePerPage, rowsPerPage }) => {
   // ** Context
   const permissao = useContext(PermissaoContext)
+  const { t } = useTranslation()
 
   return (
     <div className="w-100 py-2">
       <Row>
         <Col lg="6" className="d-flex align-items-center px-0 px-lg-1">
           <div className="d-flex align-items-center me-2">
-            <label htmlFor="rows-per-page">Mostrar</label>
+            <label htmlFor="rows-per-page">{t('Mostrar')}</label>
             <Input
               type="select"
               id="rows-per-page"
@@ -65,9 +67,9 @@ const CustomHeader = ({ handleFilter, value, handlePerPage, rowsPerPage }) => {
             tag={Link}
             to="/bloqueio_quarto/add"
             color="primary"
-            disabled={!permissao.can("create", "bloqueio_quarto")}
+            disabled={!permissao.can('create', 'bloqueio_quarto')}
           >
-            Novo bloqueio
+            {t('Novo bloqueio')}
           </Button>
         </Col>
         <Col
@@ -75,14 +77,14 @@ const CustomHeader = ({ handleFilter, value, handlePerPage, rowsPerPage }) => {
           className="actions-right d-flex align-items-center justify-content-lg-end flex-lg-nowrap flex-wrap mt-lg-0 mt-1 pe-lg-1 p-0"
         >
           <div className="d-flex align-items-center">
-            <label htmlFor="txtPesquisa">Pesquisa</label>
+            <label htmlFor="txtPesquisa">{t('Pesquisa')}</label>
             <Input
               id="txtPesquisa"
               className="ms-50 me-2 w-100"
               type="text"
               value={value}
               onChange={(e) => handleFilter(e.target.value)}
-              placeholder="Filtrar..."
+              placeholder={t('Filtrar...')}
             />
           </div>
         </Col>
@@ -97,16 +99,17 @@ const BloqueioQuartoList = () => {
   const store = useSelector((state) => state.bloqueio_quarto)
 
   // ** States
-  const [value, setValue] = useState(store.params.q ?? "")
-  const [sort, setSort] = useState(store.params.sort ?? "desc")
-  const [sortColumn, setSortColumn] = useState(store.params.sortColumn ?? "id")
+  const [value, setValue] = useState(store.params.q ?? '')
+  const [sort, setSort] = useState(store.params.sort ?? 'desc')
+  const [sortColumn, setSortColumn] = useState(store.params.sortColumn ?? 'id')
   const [currentPage, setCurrentPage] = useState(store.params.page ?? 1)
   const [rowsPerPage, setRowsPerPage] = useState(store.params.perPage ?? 10)
   const vTimeoutPesquisa = useRef()
   const [vPesquisando, setPesquisando] = useState(true)
+  const { t } = useTranslation()
 
   // ** Guardar o Cliente selecionado para atualizar a página caso mude
-  const sClienteId = localStorage.getItem("clienteId")
+  const sClienteId = localStorage.getItem('clienteId')
 
   if (vPesquisando && store.total >= 0) {
     setPesquisando(false)
@@ -193,18 +196,18 @@ const BloqueioQuartoList = () => {
         pageCount={count || 1}
         activeClassName="active"
         breakClassName="page-item"
-        pageClassName={"page-item"}
+        pageClassName={'page-item'}
         breakLinkClassName="page-link"
-        nextLinkClassName={"page-link"}
-        pageLinkClassName={"page-link"}
-        nextClassName={"page-item next"}
-        previousLinkClassName={"page-link"}
-        previousClassName={"page-item prev"}
+        nextLinkClassName={'page-link'}
+        pageLinkClassName={'page-link'}
+        nextClassName={'page-item next'}
+        previousLinkClassName={'page-link'}
+        previousClassName={'page-item prev'}
         onPageChange={(page) => handlePagination(page)}
         forcePage={
           currentPage !== 0 ? (currentPage <= count ? currentPage - 1 : 0) : 0
         }
-        containerClassName={"pagination react-paginate justify-content-end p-1"}
+        containerClassName={'pagination react-paginate justify-content-end p-1'}
       />
     )
   }
@@ -235,19 +238,19 @@ const BloqueioQuartoList = () => {
   // ** Modal de exclusão
   const handleDeleteConfirmation = (row) => {
     return MySwal.fire({
-      title: "Tem certeza?",
-      text: "Sua ação não poderá ser revertida!",
-      icon: "warning",
+      title: 'Tem certeza?',
+      text: 'Sua ação não poderá ser revertida!',
+      icon: 'warning',
       showCancelButton: true,
-      confirmButtonText: "Sim, remover!",
-      cancelButtonText: "Cancelar",
+      confirmButtonText: 'Sim, remover!',
+      cancelButtonText: 'Cancelar',
       customClass: {
-        confirmButton: "btn btn-primary",
-        cancelButton: "btn btn-outline-danger ms-1",
-        popup: "animate__animated animate__fadeIn",
+        confirmButton: 'btn btn-primary',
+        cancelButton: 'btn btn-outline-danger ms-1',
+        popup: 'animate__animated animate__fadeIn',
       },
       hideClass: {
-        popup: "animate__animated animate__zoomOut",
+        popup: 'animate__animated animate__zoomOut',
       },
       buttonsStyling: false,
     }).then(async (result) => {
@@ -255,8 +258,8 @@ const BloqueioQuartoList = () => {
         await dispatch(deleteBloqueioQuarto(row.id))
         handleFilter(store.params.q)
 
-        toast.success("Removido com sucesso!", {
-          position: "bottom-right",
+        toast.success('Removido com sucesso!', {
+          position: 'bottom-right',
         })
       }
     })
@@ -265,8 +268,8 @@ const BloqueioQuartoList = () => {
   // ** Table columns
   const columns = [
     {
-      name: "Quarto",
-      minWidth: "450px",
+      name: 'Quarto',
+      minWidth: '450px',
       sortable: true,
       selector: (row) => row.nome,
       cell: (row) => {
@@ -287,8 +290,8 @@ const BloqueioQuartoList = () => {
       },
     },
     {
-      name: <div className="text-end w-100">Ações</div>,
-      minWidth: "80px",
+      name: <div className="text-end w-100">{t('Ações')}</div>,
+      minWidth: '80px',
       cell: (row) => (
         <div className="text-end w-100">
           <div className="column-action d-inline-flex">
@@ -317,7 +320,7 @@ const BloqueioQuartoList = () => {
                   }}
                 >
                   <Trash size={14} className="me-50" />
-                  <span className="align-middle">Remover</span>
+                  <span className="align-middle">{t('Remover')}</span>
                 </DropdownItem>
               </DropdownMenu>
             </UncontrolledDropdown>
